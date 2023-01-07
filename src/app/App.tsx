4d3455a8c0a91ecd869bab9a46e1fb11105487e6
fcole90 <fcole90@gmail.com>
2023-01-07 11:37:30 +0100

    Fix layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,29 @@
+import { ThemeProvider, Typography } from "@mui/material"
+import { Stack } from "@mui/system"
+import theme from "../theming/theme"
 import CreateJoinDialog from "./CreateJoinDialog/CreateJoinDialog"
 
 const App = () => {
   return (
-    <div style={{
-      padding: '2rem',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      maxWidth: '1200px',
-      
-      display: 'flex',
-      flexGrow: 1,
-      flexDirection: 'column',
-    }}>
-      <h1>
-        Serverless WebRTC React
-      </h1>
-      <CreateJoinDialog />
-    </div>
+    <ThemeProvider theme={theme}>
+      <Stack
+        maxWidth='1200px'
+        marginX='auto'
+        minHeight='100vh'
+
+        direction='column'
+        flexGrow={1}
+
+        padding='2rem'
+      >
+        <Typography variant='h1' gutterBottom>
+          Serverless WebRTC React
+        </Typography>
+        <Stack height='100%' direction='column' flexGrow={1} alignItems='center'>
+          <CreateJoinDialog />
+        </Stack>
+      </Stack>
+    </ThemeProvider>
     
   )
 }
